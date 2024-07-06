292df099164956cf5e1a85e6a9ac65be8e2dfeed
吴博 <wubo_aaa@163.com>
2024-07-06 15:00:03 +0800

    init

--- src/main/java/cn/wubo/easy/ai/dto/FileStorageDTO.java
+++ src/main/java/cn/wubo/easy/ai/dto/FileStorageDTO.java
@@ -3,6 +3,7 @@ package cn.wubo.easy.ai.dto;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class FileStorageDTO {
@@ -13,4 +14,5 @@ public class FileStorageDTO {
     /* 00 上传 10 文档拆分中 20 文档拆分完 30 向量存储中 40向量存储完 */
     private String state;
     private Date updateTime;
+    private List<DocumentContentDTO> documentContentList;
 }
